0366f958dc4c14dd391ceab051ea4e6b7780efff
Fatima-Kashif <kashiffatima465@gmail.com>
2024-07-03 23:32:25 +0500

    Files updated

--- main.js
+++ main.js
@@ -1,69 +1,73 @@
-// for server initialize 
+// for server initialize
 import mysql from "mysql2";
 import express from "express";
-import  bodyParser from "body-parser";
-const app = express()
-const port = 3000
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
 // ye ek middleware ha middleware basically fn hota ha isse ham public folder access krte hain
-app.use(express.static('public')) 
-//aacept json body 
+app.use(express.static("public"));
+//aacept json body
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Database connection pool
 const connection = mysql.createPool({
   host: "156.67.73.151",
   user: "u481156254_khan",
   password: "#Xm8OmLB",
-  database:"u481156254_BloodBank",
-  port:"3306",
+  database: "u481156254_BloodBank",
+  port: "3306",
   waitForConnections: true,
-  connectionLimit : 10,
-  queueLimit:0
-
+  connectionLimit: 10,
+  queueLimit: 0,
 });
-let logindetails = []
-let donorDetails = []
-let patientsDetails = []
+let logindetails = [];
+let donorDetails = [];
+let patientsDetails = [];
 
-const patientUnionDonor =  "SELECT * FROM PATIENT UNION SELECT * FROM DONOR";
-const donorDetailsSql = "SELECT * FROM DONOR"
-const patientsDetailsSql = "SELECT * FROM PATIENT"
+const patientUnionDonor = "SELECT * FROM PATIENT UNION SELECT * FROM DONOR";
+const donorDetailsSql = "SELECT * FROM DONOR";
+const patientsDetailsSql = "SELECT * FROM PATIENT";
+const getAllRequest = "SELECT * FROM REQUEST";
+const getAllDonors = "SELECT * FROM DONOR_RECORD";
 connection.query(patientUnionDonor, (error, results, fields) => {
   if (error) {
-    console.error('Error connecting to MySQL:', error);
+    console.error("Error connecting to MySQL:", error);
     return;
   }
-   logindetails = results
+  logindetails = results;
   // console.log('Query results:', logindetails);
 });
 
 connection.query(donorDetailsSql, (error, results, fields) => {
   if (error) {
-    console.error('Error connecting to MySQL:', error);
+    console.error("Error connecting to MySQL:", error);
     return;
   }
-   donorDetails = results
+  donorDetails = results;
   // console.log('Query results:', donorDetails);
 });
 
 connection.query(patientsDetailsSql, (error, results, fields) => {
   if (error) {
-    console.error('Error connecting to MySQL:', error);
+    console.error("Error connecting to MySQL:", error);
     return;
   }
-   patientsDetails = results
+  patientsDetails = results;
   // console.log('Query results:', logindetails);
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.get('/login', (req, res) => {
-  res.render("login")
-})
+app.get("/", (req, res) => {
+  res.render("welcome");
+});
 
 // app.post('/loginapi', (req, res) => {
-//   let userEmail = req.body.email
+//   let userEmail  req.body.email
 //   let userPass = req.body.password
 
 //   donorDetails.forEach(element => {
@@ -82,54 +86,101 @@ app.get('/login', (req, res) => {
 //     }
 //   });
 // });
-app.post('/loginapi', (req, res) => {
-  let userEmail = req.body.email
-  let userPass = req.body.password
+app.post("/loginapi", (req, res) => {
+  let userEmail = req.body.email;
+  let userPass = req.body.password;
 
-  donorDetails.forEach(element => {
-    if(element.D_EMAIL == userEmail && element.D_PSWD == userPass){
+  donorDetails.forEach((element) => {
+    if (element.D_EMAIL == userEmail && element.D_PSWD == userPass) {
       // res.render("form")
-      res.render("formpatient",{email:element.D_EMAIL.toLowerCase(),donorID:element.DONOR_ID,d:"Donor"})
-
+      res.render("formpatient", {
+        email: element.D_EMAIL.toLowerCase(),
+        donorID: element.DONOR_ID,
+        d: "Donor",
+        formAction: "/donorapi",
+      });
     }
   });
 
-  patientsDetails.forEach(e => {
-    if(e.P_EMAIL == userEmail && e.P_PSWD == userPass){
+  patientsDetails.forEach((e) => {
+    if (e.P_EMAIL == userEmail && e.P_PSWD == userPass) {
       // res.render("form")
-      res.render("formpatient",{email:e.P_EMAIL.toLowerCase(),patientID:e.PATIENT_ID,p:"Patient"})
-
+      res.render("formpatient", {
+        email: e.P_EMAIL.toLowerCase(),
+        patientID: e.PATIENT_ID,
+        p: "Patient",
+        formAction: "/patientapi",
+      });
     }
   });
-  app.post('/patientapi', (req, res) => {
-    let bld_grp = req.body.bld_grp
-    let units = req.body.units
-    let date
-    let PT_ID = req.body.PT_ID
-      // ab ham patient ka data request me put kraha hain
-    console.log(bld_grp);
-    let patientReqSql = "INSERT INTO REQUEST (REQ_ID,PATIENT_ID,UNITS,BLOOD_GRP_REQ,TO_DATE,STATUS) VALUES(?,?,?,?,?,?)"
-    let curDate = "SELECT curdate() FROM DUAL;"
+  app.post("/patientapi", (req, res) => {
+    let bld_grp = req.body.bld_grp;
+    let units = req.body.units;
+    let date;
+    let PT_ID = req.body.PT_ID;
+    // ab ham patient ka data request me put kraha hain
+    console.log(req.body);
+    let patientReqSql =
+      "INSERT INTO REQUEST (PATIENT_ID,UNITS,BLOOD_GRP_REQ,TO_DATE,STATUS) VALUES(?,?,?,?,?)";
+    let curDate = "SELECT curdate() FROM DUAL;";
     connection.query(curDate, (error, results, fields) => {
       if (error) {
-        console.error('Error connecting to MySQL:', error);
+        console.error("Error connecting to MySQL:", error);
         return;
       }
-       date = results
-      
+      date = results;
     });
 
-    connection.query(patientReqSql,[11,parseInt(PT_ID),units,bld_grp,date,"Pending"], (error, results, fields) => {
+    connection.query(
+      patientReqSql,
+      [ parseInt(PT_ID), units, bld_grp, date, "Pending"],
+      (error, results, fields) => {
+        if (error) {
+          console.error("Error connecting to MySQL:", error);
+          return;
+        }
+        console.log(results, fields, "results, fields");
+      }
+    );
+
+    res.render("clock");
+  });
+
+  app.post("/donorapi", (req, res) => {
+    let bld_grp = req.body.bld_grp;
+    let units = req.body.units;
+    let date;
+    let DONOR_ID = req.body.DONOR_ID;
+    // ab ham patient ka data request me put kraha hain
+    console.log(req.body);
+    let donorReqSql =
+      "INSERT INTO DONOR_RECORD (DONOR_ID,D_UNITS,D_DATE_DON) VALUES(?,?,?)";
+    let curDate = "SELECT curdate() FROM DUAL;";
+    connection.query(curDate, (error, results, fields) => {
       if (error) {
-        console.error('Error connecting to MySQL:', error);
+        console.error("Error connecting to MySQL:", error);
         return;
       }
-      
-      
+      date = results;
     });
 
-    res.render("login")
+    connection.query(
+      donorReqSql,
+      [parseInt(DONOR_ID), units, new Date()],
+      (error, results, fields) => {
+        if (error) {
+          console.error("Error connecting to MySQL:", error);
+    res.render("error", {
+      errorMessage: error?.message
     });
+    return;
+        }
+        console.log(results, fields, "results, fields");
+    res.render("clock");
+  }
+    );
+
+  });
   // connection.query(query, [email, password, email, password], (err, results) => {
   //   if (err) throw err;
 
@@ -144,37 +195,60 @@ app.post('/loginapi', (req, res) => {
 });
 
 // Route for displaying form page
-app.get('/form', (req, res) => {
-  res.render('formpatient');
+app.get("/form", (req, res) => {
+  res.render("formpatient");
+}); 
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
 });
+
 
 // Route for displaying form page
-app.get('/submit', (req, res) => {
-  res.render('clock');
+app.get("/submit", (req, res) => {
+  res.render("clock");
 });
 
+app.get("/get-all-requests", (req, res) => {
+  connection.query(getAllRequest, (error, results, fields) => {
+    if (error) {
+      console.error("Error connecting to MySQL:", error);
+      return;
+    }
+    res.json(results);
+    // console.log('Query results:', logindetails);
+  });
+});
 
-  
+app.get("/get-all-donors", (req, res) => {
+  connection.query(getAllDonors, (error, results, fields) => {
+    if (error) {
+      console.error("Error connecting to MySQL:", error);
+      return;
+    }
+    res.json(results);
+    // console.log('Query results:', logindetails);
+  });
+});
+
 //trying gpt code to send data from the form to database
 
 // Route for handling form submission
-app.post('/submit', (req, res) => {
+app.post("/submit", (req, res) => {
   const { email, units, blood_group } = req.body; // Extract form data
 
-  console.log("req.body",req.body)
+  console.log("req.body", req.body);
 
   // Query to find if user is a patient or donor
   const query = `
     SELECT 'PATIENT' AS role, PATIENT_ID AS id FROM PATIENT WHERE P_EMAIL = ?
     UNION
     SELECT 'DONOR' AS role, DONOR_ID AS id FROM DONOR WHERE D_EMAIL = ?
-    `
-  ;
-
+    `;
   connection.query(query, [email, email], (err, results) => {
     if (err) throw err;
 
-    console.log(results)
+    console.log(results);
     // if (results.length > 0) {
     //   const user = results[0];
 
@@ -211,8 +285,6 @@ app.post('/submit', (req, res) => {
   });
 });
 
-
-
 // connection.query('SELECT * FROM DONOR', (error, results, fields) => {
 //   if (error) {
 //     console.error('Error connecting to MySQL:', error);
@@ -221,6 +293,6 @@ app.post('/submit', (req, res) => {
 //   console.log('Query results:', results);
 // });
 
-  app.listen(port, () => {
-    console.log("Example app listening on port ${port}")
-  }) 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
